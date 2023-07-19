a1059f5d7e0b2722ef65942406e7cc02f36e8723
Tommy Yankowski <139814400+tommyyank@users.noreply.github.com>
2023-07-18 20:56:13 -0400

    Update pizzaframe.java

--- pizzaframe.java
+++ pizzaframe.java
@@ -15,7 +15,8 @@ public class pizzaframe extends JFrame implements  ItemListener, ListSelectionLi
   double pprice = 0.00;
   double tprice = 0.00;
   double topprice;
- String origprice = "0.00";
+  String origprice = "0.00";
+  
   double s = 7.00;
   double m = 9.00;
   double l = 11.00;
@@ -31,12 +32,16 @@ public class pizzaframe extends JFrame implements  ItemListener, ListSelectionLi
   JRadioButton medium = new JRadioButton("Medium($9)");
   JRadioButton large = new JRadioButton("Large($11)");
   JRadioButton xlarge = new JRadioButton("X-Large($13)");
+  
+  
   ButtonGroup group = new ButtonGroup();//user can only select one of the buttons now they are in a group
   String[] toppings = {"Eggplant", "Green Peppers", "Hot Peppers", "Pepperoni", "Sausage", "Mushrooms", "Anchovies"};
   JList<String> toplist = new JList<String>(toppings);
   JLabel price = new JLabel(" Price of your pizza is $" + origprice);
   JCheckBox wings = new JCheckBox("Side Order Hot Wings - add $5.00");
   Container c;
+  
+  
   BorderLayout bord = new BorderLayout();
   JPanel north = new JPanel();
   JPanel center = new JPanel();
@@ -53,27 +58,32 @@ public class pizzaframe extends JFrame implements  ItemListener, ListSelectionLi
      setSize(550,250);
      c = getContentPane();
      c.setLayout(bord);
+     
      title.setFont(titlefont);
      title.setHorizontalAlignment(JLabel.CENTER);
      title.setForeground(Color.red);
-    instructions.setHorizontalAlignment(JLabel.CENTER);
-    instructions.setForeground(Color.red);
+     
+     instructions.setHorizontalAlignment(JLabel.CENTER);
+     instructions.setForeground(Color.red);
      north.setLayout(northgrid);
      north.setBackground(Color.yellow);
      north.add(title);
      north.add(instructions);
+     
+     
      c.add(north, bord.NORTH);
-    center.setLayout(centergrid);
-    group.add(small);
-    group.add(medium);
-    group.add(large);
-    group.add(xlarge);
-    center.add(small);
-    center.add(medium);
-    center.add(large);
-    center.add(xlarge);
-    toplist.setForeground(Color.red);
-    center.add(toplist);
+     center.setLayout(centergrid);
+     group.add(small);
+     group.add(medium);
+     group.add(large);
+     group.add(xlarge);
+     
+     center.add(small);
+     center.add(medium);
+     center.add(large);
+     center.add(xlarge);
+     toplist.setForeground(Color.red);
+     center.add(toplist);
     
     c.add(center, bord.CENTER);//add the panel to the BorderLayout
     south.setLayout(southgrid);
@@ -105,51 +115,49 @@ public class pizzaframe extends JFrame implements  ItemListener, ListSelectionLi
     }
     else if(medium.isSelected())
     {
-              pprice = m;
+        pprice = m;
     
     }
     else if(large.isSelected())
     {
-      pprice = l;
+        pprice = l;
      
     }
     else if(xlarge.isSelected())
     {
-      pprice = xl;
+        pprice = xl;
    
     }
       if(wings.isSelected())
-      {  pprice += w;
-     
+      {  
+        pprice += w;
       }
-tprice = topprice + pprice;
+ tprice = topprice + pprice;
  NumberFormat curnum;
-  curnum = NumberFormat.getCurrencyInstance();
-total = curnum.format(tprice);
+ curnum = NumberFormat.getCurrencyInstance();
+ total = curnum.format(tprice);
  
-      price.setText( " Price of your pizza is  " + total);//updates the JLabel declared above
+ price.setText( " Price of your pizza is  " + total);//updates the JLabel declared above
   }
   public void valueChanged(ListSelectionEvent e)
   {
     int i;
     int[] toppingselect;
     toppingselect = toplist.getSelectedIndices();
-     topprice = 0.00;
+    topprice = 0.00;
     for(i = 0; i < toppingselect.length; i++)
-    {
-        
+      { 
         topprice += 1.00;
-       
-    }
-        tprice = topprice + pprice;
-         NumberFormat curnum;
-  curnum = NumberFormat.getCurrencyInstance();
-        total = curnum.format(tprice);//will format the price value to a proper currency form
-          price.setText( " Price of your pizza is  " + total);
+      }
+    tprice = topprice + pprice;
+    NumberFormat curnum;
+    curnum = NumberFormat.getCurrencyInstance();
+    total = curnum.format(tprice);//will format the price value to a proper currency form
+    price.setText( " Price of your pizza is  " + total);
       
     
      
-  }
+   }
   public static void main(String[] args)
   {
     pizzaframe f = new pizzaframe();
